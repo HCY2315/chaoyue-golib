1d1c298058b32a6a6b28832e8102e05935184b8f
chaoyue.hou <chaoyue.hou@gmail.com>
2022-06-11 23:33:36 +0800

    [REV] modify time syntax

--- gin/middleware/access_log.go
+++ gin/middleware/access_log.go
@@ -11,7 +11,7 @@ func BuildAccessLogMiddleware(logTag string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		start := time.Now()
 		c.Next()
-		latency := time.Now().Sub(start) / time.Millisecond
+		latency := time.Since(start) / time.Millisecond
 		log.Infof("%s %d %d %s %v", logTag, c.Writer.Status(), latency, c.Request.Method, c.Request.URL)
 	}
 }
